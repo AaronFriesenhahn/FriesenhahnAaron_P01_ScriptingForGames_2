b0d3bd6c3ae90ca68b2b2a57defd2e2473e1bb9f
Aaron Friesenhahn <aaron.subzero@yahoo.com>
2021-09-24 19:43:52 -0500

    P01B v3
    Final edits and polishing of project.

--- Assets/Scripts/Killer.cs
+++ Assets/Scripts/Killer.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class Killer : Enemy
 {
+    float speed = 2.5f;
+
+    public void Awake()
+    {
+        GameObject PlayerFound = GameObject.FindGameObjectWithTag("Player");
+        _PlayerTransform = PlayerFound.transform;
+    }
+
+    protected override void Move()
+    {
+        var targetPosition = new Vector3(_PlayerTransform.position.x, transform.position.y, _PlayerTransform.position.z);
+        // Move our position a step closer to the target.
+        float step = speed * Time.deltaTime; // calculate distance to move
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
+    }
 
 }
--- README.txt
+++ README.txt
@@ -8,3 +8,5 @@ Innovations:
 Projectiles aim for Red Enemies, might create some weird projectile movements.
 Created Walls that break after 3 hits with player projectiles.
 Red Barrier that is destroyed after the Red Enemies are defeated.
+Health Bar flashes between primary color (red for player, purple for boss) and white when the Player or Boss is
+	below 50% health.
